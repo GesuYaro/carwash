561d7e0bf93f0fa87c2bb3db6ec245be6aa07b59
Semyon Shagiev <shagiev.2002@mail.ru>
2022-08-07 23:51:07 +0300

    fix N+1 problem

--- src/main/java/shagiev/carwash/repo/AvailableIntervalRepo.java
+++ src/main/java/shagiev/carwash/repo/AvailableIntervalRepo.java
@@ -1,8 +1,53 @@
 package shagiev.carwash.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import shagiev.carwash.model.availableinterval.AvailableInterval;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface AvailableIntervalRepo extends JpaRepository<AvailableInterval, Long>, JpaSpecificationExecutor<AvailableInterval> {
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    List<AvailableInterval> findAll();
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    List<AvailableInterval> findAll(Specification<AvailableInterval> spec);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    Page<AvailableInterval> findAll(Specification<AvailableInterval> spec, Pageable pageable);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    Page<AvailableInterval> findAll(Pageable pageable);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    Optional<AvailableInterval> findById(Long aLong);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    boolean existsById(Long aLong);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "carBox")
+    boolean exists(Specification<AvailableInterval> spec);
 }
--- src/main/java/shagiev/carwash/repo/EntryRepo.java
+++ src/main/java/shagiev/carwash/repo/EntryRepo.java
@@ -1,10 +1,53 @@
 package shagiev.carwash.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import shagiev.carwash.model.entry.Entry;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EntryRepo extends JpaRepository<Entry, Long>, JpaSpecificationExecutor<Entry> {
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    List<Entry> findAll();
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    List<Entry> findAll(Specification<Entry> spec);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    Page<Entry> findAll(Specification<Entry> spec, Pageable pageable);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    boolean exists(Specification<Entry> spec);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    Page<Entry> findAll(Pageable pageable);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    Optional<Entry> findById(Long aLong);
+
+    @Override
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"carBox", "carwashService"})
+    boolean existsById(Long aLong);
 }
